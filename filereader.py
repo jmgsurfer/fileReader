ec884c3ad77c86e86954285438917971892ee612
jmgsurfer <gsurfer@free.Fr>
2022-01-01 18:33:54 +0100

    Append check-if-binary function and hexa display

--- filereader.py
+++ filereader.py
@@ -1,12 +1,16 @@
-import string
-
+import sys
+import binascii
+#
+# check if binary
 textchars = bytearray({7,8,9,10,12,13,27} | set(range(0x20, 0x100)) - {0x7f})
 is_binary_string = lambda bytes: bool(bytes.translate(None, textchars))
-
-if is_binary_string(open('test.exe', 'rb').read(1024)):
-    with open("test.exe", "rb") as f:
+#
+#
+if is_binary_string(open(sys.argv[1], 'rb').read(1024)):
+    with open(sys.argv[1], "rb") as f:
         byte = f.read(4)
-        print(byte)
+        data = binascii.hexlify(byte, ' ').decode('ascii')
+        print(data)
 
 else:
     print("text")
